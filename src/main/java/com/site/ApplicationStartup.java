45646a6cb633b16f64c06e725bb33d33b15e157b
Dishant Loriya <gitthub9999@gmail.com>
2023-09-30 15:04:34 +0530

    change application name

--- src/main/java/com/site/ApplicationStartup.java
+++ src/main/java/com/site/ApplicationStartup.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringtestApplication {
+public class ApplicationStartup {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringtestApplication.class, args);
+		SpringApplication.run(ApplicationStartup.class, args);
 	}
 
 }
